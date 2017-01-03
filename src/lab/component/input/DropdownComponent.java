96ec0e917aad4a264ddf298040ffb1edf3df115c
funkitech <funkiben@gmail.com>
2017-01-03 08:43:57 -0500

    Changed some stuiff

--- src/lab/component/input/DropdownComponent.java
+++ src/lab/component/input/DropdownComponent.java
@@ -9,29 +9,32 @@ import javax.swing.JPanel;
 
 import lab.component.LabComponent;
 
-public abstract class DropdownComponent extends LabComponent implements 
-ActionListener {
+public abstract class DropdownComponent extends LabComponent implements ActionListener {
 
 	private JComboBox<String> dropdown;
 	private ArrayList<String> dropdownList;
+
 	public DropdownComponent(int width, int height, String... args) {
-		super(width,height);
+		super(width, height);
+		
 		dropdownList = new ArrayList<String>();
-		for(String c : args) {
+		
+		for (String c : args) {
 			dropdownList.add(c);
 		}
-		dropdown = new JComboBox<String>(args);
 		
+		dropdown = new JComboBox<String>(args);
+
 	}
-	
+
 	@Override
 	public void draw(int x, int y, int width, int height, Graphics g) {
-		
+
 	}
 
 	@Override
 	public void drawInputs(int x, int y, int width, int height, JPanel panel) {
-		dropdown.setSize(width,height);
+		dropdown.setSize(width, height);
 		dropdown.setLocation(x, y);
 		dropdown.addActionListener(this);
 		panel.add(dropdown);
