5ebeafa12e78ace613a62cb88f0877b36086b466
funkiben <funkiben@gmail.com>
2017-02-04 14:58:02 -0500

    Finished DataTable component

--- src/lab/component/data/DataTable.java
+++ src/lab/component/data/DataTable.java
@@ -1,5 +1,6 @@
 package lab.component.data;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
@@ -12,23 +13,24 @@ public class DataTable<E> extends LabComponent {
 	public static final int COLUMN_TITLES_ONLY = 2;
 	public static final int ROW_AND_COLUMN_TITLES = 3;
 	
-	private final int columns;
 	private final int rows;
+	private final int columns;
 	private final int type;
 	private final E[][] data;
 	private final String[] rowTitles;
 	private final String[] columnTitles;
 	private Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
 	
-	public DataTable(int width, int height, int columns, int rows, int type) {
+	@SuppressWarnings("unchecked")
+	public DataTable(int width, int height, int rows, int columns, int type) {
 		super(width, height);
 		
-		this.columns = columns;
 		this.rows = rows;
+		this.columns = columns;
 		
 		this.type = type;
 		
-		data = (E[][]) new Comparable[rows][columns];
+		data = (E[][]) new Comparable[columns][rows];
 		
 		rowTitles = new String[rows];
 		columnTitles = new String[columns];
@@ -43,31 +45,63 @@ public class DataTable<E> extends LabComponent {
 		data[x][y] = e;
 	}
 	
+	@SuppressWarnings("unchecked")
+	public void setCell(int x, int y, double d) {
+		data[x][y] = (E) new Double(d);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void setCell(int x, int y, int i) {
+		data[x][y] = (E) new Integer(i);
+	}
+	
 	public E getCell(int x, int y) {
-		return data[y][x];
+		return data[x][y];
 	}
 
 	public void setAll(E e) {
-		for (int x = 0; x < rows; x++) {
-			for (int y = 0; y < columns; y++) {
-				data[x][y] = e;
-			}
+		for (int x = 0; x < columns; x++) {
+			setRow(x, e);
+		}
+	}
+
+	public void setAll(double d) {
+		for (int x = 0; x < columns; x++) {
+			setRow(x, d);
+		}
+	}
+
+	public void setAll(int i) {
+		for (int x = 0; x < columns; x++) {
+			setRow(x, i);
 		}
 	}
 	
 	public void setRow(int row, E e) {
 		for (int i = 0; i < columns; i++) {
-			data[row][i] = e;
+			setCell(i, row, e);
 		}
 	}
 	
 	public void setColumn(int column, E e) {
 		for (int i = 0; i < rows; i++) {
-			data[i][column] = e;
+			setCell(column, i, e);
+		}
+	}
+
+	public void setRow(int row, double d) {
+		for (int i = 0; i < columns; i++) {
+			setCell(i, row, d);
 		}
 	}
 	
-	public String display(E e) {
+	public void setColumn(int column, int n) {
+		for (int i = 0; i < rows; i++) {
+			setCell(column, i, n);
+		}
+	}
+	
+	public String getString(E e) {
 		return e.toString();
 	}
 	
@@ -79,29 +113,122 @@ public class DataTable<E> extends LabComponent {
 		return font;
 	}
 	
+	public void setRowTitle(int row, String title) {
+		rowTitles[row] = title;
+	}
+	
+	public void setColumnTitle(int column, String title) {
+		columnTitles[column] = title;
+	}
+	
+	public String getRowTitle(int row) {
+		return rowTitles[row];
+	}
+	
+	public String getColumnTitle(int column) {
+		return columnTitles[column];
+	}
+	
 	@Override
 	public void draw(int x, int y, int width, int height, Graphics g) {
-		double cellWidth = 1.0 / rows * width;
-		double cellHeight = 1.0 / rows * width;
+		double cellWidth = 1.0 / columns * width;
+		double cellHeight = 1.0 / rows * height;
+		
+		g.setColor(Color.black);
 		
 		if (type == NO_TITLES) {
 			
-			for (int yi = 0; yi < columns; yi++) {
-				for (int xi = 0; xi < rows; xi++) {
+			for (int yi = 0; yi < rows; yi++) {
+				for (int xi = 0; xi < columns; xi++) {
 					
 					g.drawRect((int) (xi * cellWidth) + x, (int) (yi * cellHeight) + y, (int) cellWidth, (int) cellHeight);
 					
-					
+					if (data[xi][yi] != null) {
+						drawCenteredString(g, getString(data[xi][yi]), (int) (xi * cellWidth) + x + (int) (cellWidth / 2), (int) (yi * cellHeight) + y + (int) (cellHeight / 2));
+					}
 					
 				}
 			}
 			
 		} else if (type == ROW_TITLES_ONLY) {
 			
+			cellWidth = 1.0 / (columns + 1) * width;
+			
+			for (int yi = 0; yi < rows; yi++) {
+				for (int xi = 0; xi < columns + 1; xi++) {
+					
+					g.drawRect((int) (xi * cellWidth) + x, (int) (yi * cellHeight) + y, (int) cellWidth, (int) cellHeight);
+					
+					if (xi != 0) {
+						
+						if (data[xi - 1][yi] != null) {
+							drawCenteredString(g, getString(data[xi - 1][yi]), (int) (xi * cellWidth) + x + (int) (cellWidth / 2), (int) (yi * cellHeight) + y + (int) (cellHeight / 2));
+						}
+						
+					} else {
+						if (rowTitles[yi] != null) {
+							drawCenteredString(g, rowTitles[yi], (int) (xi * cellWidth) + x + (int) (cellWidth / 2), (int) (yi * cellHeight) + y + (int) (cellHeight / 2));
+						}
+					}
+					
+				}
+			}
+			
+			
 		} else if (type == COLUMN_TITLES_ONLY) {
 			
-		} else if (type == ROW_AND_COLUMN_TITLES) {
+			cellHeight = 1.0 / (rows + 1) * height;
 			
+			for (int yi = 0; yi < rows + 1; yi++) {
+				for (int xi = 0; xi < columns; xi++) {
+					
+					g.drawRect((int) (xi * cellWidth) + x, (int) (yi * cellHeight) + y, (int) cellWidth, (int) cellHeight);
+					
+					if (yi != 0) {
+						
+						if (data[xi][yi - 1] != null) {
+							drawCenteredString(g, getString(data[xi][yi - 1]), (int) (xi * cellWidth) + x + (int) (cellWidth / 2), (int) (yi * cellHeight) + y + (int) (cellHeight / 2));
+						}
+						
+					} else {
+						if (columnTitles[xi] != null) {
+							drawCenteredString(g, columnTitles[xi], (int) (xi * cellWidth) + x + (int) (cellWidth / 2), (int) (yi * cellHeight) + y + (int) (cellHeight / 2));
+						}
+					}
+					
+				}
+			}
+			
+		} else if (type == ROW_AND_COLUMN_TITLES) {
+			cellWidth = 1.0 / (columns + 1) * width;
+			cellHeight = 1.0 / (rows + 1) * height;
+			
+			for (int yi = 0; yi < rows + 1; yi++) {
+				for (int xi = 0; xi < columns + 1; xi++) {
+					
+					g.drawRect((int) (xi * cellWidth) + x, (int) (yi * cellHeight) + y, (int) cellWidth, (int) cellHeight);
+					
+					if (yi != 0 && xi != 0) {
+						
+						if (data[xi - 1][yi - 1] != null) {
+							drawCenteredString(g, getString(data[xi - 1][yi - 1]), (int) (xi * cellWidth) + x + (int) (cellWidth / 2), (int) (yi * cellHeight) + y + (int) (cellHeight / 2));
+						}
+						
+					} else if (xi == 0 && yi != 0) {
+						
+						if (rowTitles[yi - 1] != null) {
+							drawCenteredString(g, rowTitles[yi - 1], (int) (xi * cellWidth) + x + (int) (cellWidth / 2), (int) (yi * cellHeight) + y + (int) (cellHeight / 2));
+						}
+						
+					} else if (yi == 0 && xi != 0) {
+						
+						if (columnTitles[xi - 1] != null) {
+							drawCenteredString(g, columnTitles[xi - 1], (int) (xi * cellWidth) + x + (int) (cellWidth / 2), (int) (yi * cellHeight) + y + (int) (cellHeight / 2));
+						}
+					}
+					
+				}
+			}
 		}
 	}
 
--- src/test/DataTableTestLab.java
+++ src/test/DataTableTestLab.java
@@ -15,14 +15,32 @@ public class DataTableTestLab extends LabFrame {
 	public DataTableTestLab() {
 		super("Data Table Test Lab", 800, 800);
 		
-		DataTable<Double> dataTable = new DataTable<Double>(400, 400, 10, 10, DataTable.ROW_AND_COLUMN_TITLES) {
+		DataTable<Double> dataTable = new DataTable<Double>(400, 400, 10, 5, DataTable.ROW_AND_COLUMN_TITLES) {
 			@Override
-			public String display(Double d) {
+			public String getString(Double d) {
 				return SigFig.sigfigalize(d, 3);
 			}
 		};
 		
+		//dataTable.setAll(new Double(342543));
+		//dataTable.setCell(0, 4, new Double(232));
+		//dataTable.setColumn(2, new Double(234));
+		dataTable.setRow(2, 4435345);
+		
+		for (int i = 0; i < 5; i++) {
+			dataTable.setColumnTitle(i, "COLUMNs");
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			dataTable.setRowTitle(i, "ROWs");
+		}
+		
+		dataTable.setOffsetX(200);
+		dataTable.setOffsetY(200);
+		
 		addComponent(dataTable);
+		
+		start(60);
 		
 	}
 
