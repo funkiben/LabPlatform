9dba3758560252946589bed97f221e3e6de8ecc1
Rjected <kidscline01@gmail.com>
2017-03-03 10:52:07 -0500

    gauge

---
+++ src/lab/component/sensor/PressureGauge.java
@@ -0,0 +1,79 @@
+package lab.component.sensor;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.SwingConstants;
+
+import lab.component.LabComponent;
+import lab.component.MeasurableComponent;
+import lab.component.geo.Rectangle;
+import lab.component.swing.Label;
+import lab.util.SigFig;
+
+public class PressureGauge extends MeasurableComponent{
+
+	private Label titleText; 
+	private int sigfigs;
+	private Label gauge;
+	
+	
+	public PressureGauge(int width, int height, String title, int sigfigs) {
+		super(width, height);
+		setValue(0);
+		
+		setLayout(LabComponent.FREE_FORM);
+		
+		this.sigfigs = sigfigs;
+		
+		gauge = new Label(width/2,height/4);
+		gauge.setOffset((width-gauge.getWidth())/2, 2*(height-gauge.getHeight())/3);
+		gauge.setShowBounds(true);
+		Rectangle gaugeBorder = new Rectangle(gauge.getWidth()+10,gauge.getHeight()+10);
+		gauge.addChild(gaugeBorder);
+		gaugeBorder.setFillColor(Color.white);
+		gaugeBorder.setOffset(-5, -3);
+		gauge.getJComponent().setVerticalAlignment(SwingConstants.CENTER);
+		gauge.getJComponent().setHorizontalAlignment(SwingConstants.CENTER);
+		gauge.setWrap(true);
+		
+		titleText = new Label(width/2,height/6,"<center>"+title+"</center>");
+		titleText.setOffset((width - titleText.getWidth())/2, 5*(width - titleText.getWidth())/12);
+		titleText.setShowBounds(true);
+		Rectangle titleBorder = new Rectangle(titleText.getWidth()+5,titleText.getHeight()+5);
+		titleText.addChild(titleBorder);
+		titleBorder.setFillColor(Color.white);
+		titleBorder.setOffset(-3, -1);
+		titleText.getJComponent().setVerticalAlignment(SwingConstants.CENTER);
+		titleText.getJComponent().setHorizontalAlignment(SwingConstants.CENTER);
+		titleText.getJComponent().setFont(titleText.getJComponent().getFont().deriveFont(10f));
+		titleText.setWrap(true);
+		
+		addChild(titleText);
+		addChild(gauge);
+		
+		
+	}
+
+	@Override
+	public void draw(int x, int y, int width, int height, Graphics g) {
+		this.setValue(this.getValue()+.001);
+		
+		gauge.setText(SigFig.sigfigalize(this.getValue(),sigfigs));
+		
+		g.setColor(Color.black);
+		g.fillOval(x, y, width, height);
+		
+		int maxValue = 200;
+		int maxRuns = 10;
+		
+		for(int i = 0; i < maxRuns; i++) {
+		g.setColor(new Color((maxValue/maxRuns)*i,(maxValue/maxRuns)*i,(maxValue/maxRuns)*i));
+		g.fillOval(x+i, y+i, width-2*i, height-2*i);
+		}
+		
+		gauge.getJComponent().setFont(gauge.getJComponent().getFont().deriveFont(2*sigfigs*(float)(Math.sqrt(width*width+height*height)/Math.sqrt(gauge.getWidth()*gauge.getWidth()+gauge.getHeight()*gauge.getWidth()))));
+		
+	}
+	
+}
