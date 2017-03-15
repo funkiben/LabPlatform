c8e0c27c6ba0a82330d16c543d6dd090e8e664ae
JTisboss <jdt103@comcast.net>
2017-03-15 14:23:11 -0400

    continued work

--- src/lab/component/swing/ScrollLabel.java
+++ src/lab/component/swing/ScrollLabel.java
@@ -4,9 +4,15 @@ import java.awt.Component;
 
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class ScrollLabel extends SwingComponent{
 
+	public static final int HORIZONTAL_SCROLLBAR_ALWAYS = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS;
+	public static final int HORIZONTAL_SCROLLBAR_NEVER = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
+	public static final int HORIZONTAL_SCROLLBAR_AS_NEEDED = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED;
+
+	
 	private JLabel label;
 	private JScrollPane scrollPane;
 	
@@ -20,5 +26,12 @@ public class ScrollLabel extends SwingComponent{
 	public Component getJComponent() {
 		return scrollPane;
 	}
+	
+	public void setText(String text){
+		label.setText(text);
+	}
 
+	public void setHoriztonalScrollBarPolicy(int policy){
+		scrollPane.setHorizontalScrollBarPolicy(policy);
+	}
 }
