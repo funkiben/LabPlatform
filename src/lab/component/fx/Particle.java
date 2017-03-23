ed8a998cf3e3cc04c00f97616b0f65f6cb8aab79
funkitech <funkiben@gmail.com>
2017-03-23 14:13:58 -0400

    Started work on Particle fix

--- src/lab/component/fx/Particle.java
+++ src/lab/component/fx/Particle.java
@@ -78,41 +78,49 @@ public class Particle {
 	}
 	
 	private void checkForCollisions() {
+		checkForCollisions(position.clone(), position.add(velocity));
+	}
+	
+	private void checkForCollisions(Vector2 oldPosition, Vector2 newPosition) {
 		
-		Vector2 newPosition = position.add(velocity), p1, p2;
-		Vector2[] collisionEdge = null;
+		Vector2 intersect, p1, p2;
 		
 		for (Vector2[] edge : system.getCollidableEdges()) {
-			if (getLineIntersectionPoint(position, newPosition, edge[0], edge[1]) != null) {
-				collisionEdge = edge;
+			
+			intersect = getLineIntersectionPoint(oldPosition, newPosition, edge[0], edge[1]);
+			
+			if (intersect != null) {
+				p1 = edge[0];
+				p2 = edge[1];
+				
+				if (p1.getX() == p2.getX()) {
+					p1 = p1.add(0.0001, 0);
+				}
+				
+				if (p1.getY() == p2.getY()) {
+					p1 = p1.add(0, 0.0001);
+				}
+				
+				double m = (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
+				m = 1.0 / -m;
+				
+				Vector2 n = new Vector2(1, m);
+				
+				Vector2 u = n.multiply(velocity.dot(n) / n.dot(n));
+				Vector2 w = velocity.subtract(u);
+				
+				velocity = w.multiply(system.getFriction()).subtract(u.multiply(system.getFriction()));
+				
+				oldPosition = intersect;
+				newPosition = intersect.add(velocity);
+				
+				checkForCollisions(oldPosition, newPosition);
 				break;
 			}
 		}
 		
-		if (collisionEdge == null) {
-			return;
-		}
+		position = oldPosition;
 		
-		p1 = collisionEdge[0];
-		p2 = collisionEdge[1];
-		
-		if (p1.getX() == p2.getX()) {
-			p1 = p1.add(0.0001, 0);
-		}
-		
-		if (p1.getY() == p2.getY()) {
-			p1 = p1.add(0, 0.0001);
-		}
-		
-		double m = (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
-		m = 1.0 / -m;
-		
-		Vector2 n = new Vector2(1, m);
-		
-		Vector2 u = n.multiply(velocity.dot(n) / n.dot(n));
-		Vector2 w = velocity.subtract(u);
-		
-		velocity = w.multiply(system.getFriction()).subtract(u.multiply(system.getFriction()));
 		
 		
 	}
