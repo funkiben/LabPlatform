31d4ffe43fbe3127ca441a72502b3854fe9dde2f
JTisboss <jdt103@comcast.net>
2017-03-27 08:49:49 -0400

    Finished MenuComponent
    Gee I hope no one will ever need to use getters or setters with this
    class ;)

--- src/test/MenuTestLab.java
+++ src/test/MenuTestLab.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import lab.LabFrame;
 import lab.component.LabComponent;
 import lab.component.swing.input.MenuComponent;
@@ -17,8 +20,31 @@ public class MenuTestLab extends LabFrame {
 		getRoot().setLayout(LabComponent.PARAGRAPH);
 		MenuComponent menu = new MenuComponent(500, 20);
 		menu.addMenu("File");
-		menu.addMenuItem("Open File", "File");
-		menu.addRadioButtonMenuItem("Yes", "File", false);
+		menu.addMenuItem("Open File", "File", new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Open file");
+			}
+
+		});
+		menu.addRadioButtonMenuItem("Yes", "File", false, new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("selected");
+			}
+
+		});
+		menu.addMenu("Hi there");
+		menu.addMenuItem("Howdy bud", "Hi there", new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("gotta love those flapjacks");
+			}
+			
+		});
 		addComponent(menu);
 		start(30);
 	}
