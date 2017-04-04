1d2ea7fc6605a216e5fe7e08964d0f21d020f1e0
JTisboss <jdt103@comcast.net>
2017-04-04 08:37:34 -0400

    Added some comments

--- src/lab/component/swing/ScrollLabel.java
+++ src/lab/component/swing/ScrollLabel.java
@@ -14,42 +14,44 @@ import javax.swing.ScrollPaneConstants;
 
 import lab.component.swing.input.FontStyle;
 
+//creates a label with a scroll bar that reads from a text file
 public class ScrollLabel extends SwingComponent {
 
+	// create class constants so user doesn't have to import ScrollPaneConstants
 	public static final int HORIZONTAL_SCROLLBAR_ALWAYS = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS;
 	public static final int HORIZONTAL_SCROLLBAR_NEVER = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
 	public static final int HORIZONTAL_SCROLLBAR_AS_NEEDED = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED;
 
+	// create components
 	private final JLabel label;
 	private final JScrollPane scrollPane;
 
+	// initialize components
 	public ScrollLabel(int width, int height, String filePath) {
 		super(width, height);
 		label = new JLabel(readFile(filePath));
 		scrollPane = new JScrollPane(label);
 	}
 
+	// read a file
 	private static String readFile(String path) {
 		BufferedReader bufferedReader;
-
+		// try to read text
 		try {
 			InputStream inputStream = ScrollLabel.class.getResourceAsStream(path);
 			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 			bufferedReader = new BufferedReader(inputStreamReader);
 			StringBuilder stringBuilder = new StringBuilder();
 			String line = bufferedReader.readLine();
-
 			while (line != null) {
 				stringBuilder.append(line);
 				stringBuilder.append(System.lineSeparator());
 				line = bufferedReader.readLine();
 			}
 			return stringBuilder.toString();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		return null;
 	}
 
