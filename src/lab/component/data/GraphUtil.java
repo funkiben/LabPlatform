35f7478d37633697662b1e050cb7c3157cd5ccf7
funkitech <funkiben@gmail.com>
2017-04-06 10:40:28 -0400

    Added getLineOfBestFit to GraphUtil

--- src/lab/component/data/GraphUtil.java
+++ src/lab/component/data/GraphUtil.java
@@ -1,5 +1,35 @@
 package lab.component.data;
 
+import java.util.List;
+
+import lab.util.Vector2;
+
 public class GraphUtil {
+	
+		
+		int n = points.size();
+		
+		double meanX = 0, meanY = 0, num = 0, denom = 0, a = 0, m;
+		
+		for (Vector2 v : points) {
+			meanX += v.getX();
+			meanY += v.getY();
+		}
+		
+		meanX /= n;
+		meanY /= n;
+		
+		for (Vector2 v : points) {
+			a = v.getX() - meanX;
+			num += a * (v.getY() - meanY);
+			denom += a * a;
+		}
+		
+		m = num / denom;
+		
+		return new double[] { m, meanY - m * meanX };
+		
+	}
+	
 	
 }
